738db24555b62cb91a4f80037278f9b0ce741a84
bombbie <bombbie@gmail.com>
2024-05-26 22:42:18 +0900

    add ParentDocumentRetriever

--- backend/apps/rag/faiss/faiss_client.py
+++ backend/apps/rag/faiss/faiss_client.py
@@ -23,16 +23,18 @@ import logging
 import uuid
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
+from langchain.storage import InMemoryStore
 from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate, PromptTemplate
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_community.vectorstores import FAISS
+from langchain_community.vectorstores.neo4j_vector import SearchType
 from langchain_community.vectorstores.utils import DistanceStrategy
 from langchain_community.embeddings import OllamaEmbeddings, HuggingFaceEmbeddings
 from langchain_community.chat_models import ChatOllama
 from langchain.retrievers.multi_query import MultiQueryRetriever
-from langchain.retrievers import EnsembleRetriever, RePhraseQueryRetriever, ContextualCompressionRetriever
+from langchain.retrievers import EnsembleRetriever, RePhraseQueryRetriever, ContextualCompressionRetriever, ParentDocumentRetriever
 from langchain.retrievers.document_compressors import LLMChainExtractor, LLMChainFilter
 from langchain_community.retrievers import BM25Retriever
 from langchain.text_splitter import RecursiveCharacterTextSplitter
@@ -50,6 +52,10 @@ load_dotenv()
 
 CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 1500))
 CHUNK_OVERLAP = int(os.environ.get("CHUNK_OVERLAP", 100))
+PARENT_CHUNK_SIZE = int(os.environ.get("PARENT_CHUNK_SIZE", 2000))
+PARENT_CHUNK_OVERLAP = int(os.environ.get("PARENT_CHUNK_OVERLAP", 100))
+CHILD_CHUNK_SIZE = int(os.environ.get("CHILD_CHUNK_SIZE", 500))
+CHILD_CHUNK_OVERLAP = int(os.environ.get("CHILD_CHUNK_OVERLAP", 20))
 os_is_mac = (os.getenv("OS_SUPPORT") == "MAC") 
 
 def calculate_sha256(file_path):
@@ -58,7 +64,7 @@ def calculate_sha256(file_path):
         for byte_block in iter(lambda: f.read(4096), b""):
             sha256_hash.update(byte_block)
     #
-    logging.info(f"{file_path}의 sha256 값 : {sha256_hash.hexdigest()}")
+    # logging.info(f"{file_path}의 sha256 값 : {sha256_hash.hexdigest()}")
     #
     return sha256_hash.hexdigest()
 
@@ -83,54 +89,16 @@ class FAISS_CLIENT:
             )
             
         self.collections = {}
-        self.load_collections()
+        # self.load_collections()
 
-    def load_collections(self):
-        """Load all FAISS indexes from the vector_db_dir."""
 
-        for file in os.listdir(self.vector_db_dir):
-            if file.endswith(".faiss"):
-                index_name = file.replace(".faiss", "")
-                faiss_db = FAISS.load_local(
-                    self.vector_db_dir, 
-                    self.ollama['embedding'],
-                    index_name,
-                    allow_dangerous_deserialization=True,
-                    # kwargs={"distance_strategy":DistanceStrategy.COSINE,}
-                    distance_strategy=DistanceStrategy.COSINE
-                )
-                retriever = self.get_retriever(faiss_db, type='vectordb')
-                rag_chain = self.create_rag_chain(retriever) if retriever else None
-                template_string = (
-                    "Given the following question and context, return YES if the context is relevant to the question and NO if it isn't.\n\n"
-                    "Answer with YES or NO only. No additional text.\n\n"
-                    "> Question: {question}\n"
-                    "> Context:\n"
-                    ">>>\n"
-                    "{context}\n"
-                    ">>>\n"
-                    "> Relevant (YES / NO):"
-                )
-                prompt = PromptTemplate.from_template(template=template_string)
-                compression_retriever = ContextualCompressionRetriever(
-                    # base_compressor= LLMChainExtractor.from_llm(self.ollama["llm"]),
-                    base_compressor= LLMChainFilter.from_llm(self.ollama["llm"], prompt),
-                    base_retriever = retriever,
-                )
-                rag_chain_compression = self.create_rag_chain(compression_retriever) if compression_retriever else None
-                self.collections[index_name] = {
-                    "faiss_db": faiss_db,
-                    "retriever": retriever,
-                    "compression_retriever": compression_retriever,
-                    "rag_chain" : rag_chain,
-                    # IMPORTANT:
-                    # NOTE: MEMORY: retriever기반의 rag_chain은 거의 답변이 동일함 (local vs openai)
-                    # CAUTION: 단지 시간소요가 많이됨
-                    "rag_chain_compression":rag_chain_compression
-                }
-                #
-                logging.info(f"adding collection[{index_name}]")
-                #
+    def load_collection_by_ext(self, directory, ext):
+        """Load collections for all files with the specified extension in the given directory."""
+        self.doc_dir = directory
+        for file in os.listdir(directory):
+            if file.endswith(ext):
+                file_path = os.path.join(directory, file)
+                self.create_collection(file_path)
 
     def create_collection(self, src_pdf_path):
         """Create a FAISS collection from a PDF file."""
@@ -145,26 +113,7 @@ class FAISS_CLIENT:
             # raise ValueError(f"Collection with name {index_name} already exists.")
             return index_name
 
-        # loader = PDFPlumberLoader(src_pdf_path)
-        loader = PyPDFLoader(src_pdf_path)
-        _documents = loader.load()
-        text_splitter = RecursiveCharacterTextSplitter(chunk_size=CHUNK_SIZE, chunk_overlap=CHUNK_OVERLAP)
-        docs = text_splitter.split_documents(_documents)
-
-        embeddings = self.ollama['embedding']
-        texts = [doc.page_content.replace("\n", " ") for doc in docs]
-        metadatas = [{"collection_name": index_name, **doc.metadata} for doc in docs]
-        ids = [str(uuid.uuid1()) for _ in texts]
-        text_embeddings = embeddings.embed_documents(texts)
-        text_embedding_pairs = zip(texts, text_embeddings)
-
-        faiss_db = FAISS.from_embeddings(
-            text_embedding_pairs, 
-            embedding=embeddings, 
-            ids=ids, metadatas=metadatas)
-        faiss_db.save_local(self.vector_db_dir, index_name)
-
-        retriever = self.get_retriever(faiss_db, type='vectordb')
+        faiss_db, retriever = self.create_faiss_db_and_retriever(src_pdf_path, index_name)
         rag_chain = self.create_rag_chain(retriever) if retriever else None
         compression_retriever = ContextualCompressionRetriever(
                     # base_compressor= LLMChainExtractor.from_llm(self.ollama["llm"]),
@@ -173,12 +122,18 @@ class FAISS_CLIENT:
         )
         rag_chain_compression = self.create_rag_chain(compression_retriever) if compression_retriever else None
 
+        # for ParentDocumentRetriever
+        parent_doc_retriever = self.create_faiss_db_and_parent_doc_retriever(src_pdf_path)
+        rag_chain_parent_doc = self.create_rag_chain(parent_doc_retriever) 
+
         self.collections[index_name] = {
             "faiss_db": faiss_db,
             "retriever": retriever,
             "compression_retriever": compression_retriever, 
             "rag_chain": rag_chain,
-            "rag_chain_compression": rag_chain_compression
+            "rag_chain_compression": rag_chain_compression,
+            "parent_doc_retriever": parent_doc_retriever,
+            "rag_chain_parent_doc": rag_chain_parent_doc,
         }
         
         return index_name
@@ -231,6 +186,25 @@ class FAISS_CLIENT:
                 raise ValueError(f"Collection {collection_name} does not exist.")
             retriever = self.collections[collection_name]["compression_retriever"]
             return retriever.invoke(query)
+
+    def search_parent_doc(self, query, collection_name):
+        """Search the given query in the specified collection."""
+        #
+        logging.info(f"searching using parentDocRetriever [{query}] in collection [{collection_name}]")
+        #
+        if collection_name == "all":
+            results = []
+            for name, data in self.collections.items():
+                retriever = data["parent_doc_retriever"]
+                result = retriever.invoke(query)
+                results.extend(result)
+            return results
+            # return ContextCompress(results).compress()
+        else:
+            if collection_name not in self.collections:
+                raise ValueError(f"Collection {collection_name} does not exist.")
+            retriever = self.collections[collection_name]["parent_doc_retriever"]
+            return retriever.invoke(query)
         
     def search_llm(self, query, collection_name):
         """Search the given query in the specified collection."""
@@ -268,6 +242,25 @@ class FAISS_CLIENT:
             if collection_name not in self.collections:
                 raise ValueError(f"Collection {collection_name} does not exist.")
             rag_chain = self.collections[collection_name]["rag_chain_compression"]
+            return rag_chain.invoke({'input':query})
+
+    def search_llm_parent_doc(self, query, collection_name):
+        """Search the given query in the specified collection."""
+        #
+        logging.info(f"searching using rag_chain_parent_doc [{query}] in collection [{collection_name}]")
+        #
+        if collection_name == "all":
+            results = []
+            for name, data in self.collections.items():
+                rag_chain = data["rag_chain_parent_doc"]
+                result = rag_chain.invoke(query)
+                results.extend(result)
+            return results
+            # return ContextCompress(results).compress()
+        else:
+            if collection_name not in self.collections:
+                raise ValueError(f"Collection {collection_name} does not exist.")
+            rag_chain = self.collections[collection_name]["rag_chain_parent_doc"]
             return rag_chain.invoke({'input':query})
         
     def get_collections(self):
@@ -338,3 +331,120 @@ class FAISS_CLIENT:
         #     | output_parser 
         # )
 
+    def create_faiss_db_and_retriever(self, src_pdf_path, index_name):
+        """Create a FAISS database and retriever from a PDF file."""
+
+        # loader = PDFPlumberLoader(src_pdf_path)
+        loader = PyPDFLoader(src_pdf_path)
+        _documents = loader.load()
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=CHUNK_SIZE, chunk_overlap=CHUNK_OVERLAP)
+        docs = text_splitter.split_documents(_documents)
+
+        embeddings = self.ollama['embedding']
+        texts = [doc.page_content.replace("\n", " ") for doc in docs]
+        #
+        # FIXME: TODO: 아마도 아래 metadata 셋팅하는게 필요없을 듯. 그냥 원래 doc.metadata만 있어도 될 듯
+        #
+        metadatas = [{"collection_name": index_name, **doc.metadata} for doc in docs]
+        ids = [str(uuid.uuid1()) for _ in texts]
+        text_embeddings = embeddings.embed_documents(texts)
+        text_embedding_pairs = zip(texts, text_embeddings)
+
+        faiss_db = FAISS.from_embeddings(
+            text_embedding_pairs, 
+            embedding=embeddings, 
+            ids=ids, metadatas=metadatas)
+        # faiss_db.save_local(self.vector_db_dir, metadatas[0]['collection_name'])
+
+        retriever = self.get_retriever(faiss_db, type='vectordb')
+        return faiss_db, retriever
+
+    def create_faiss_db_and_parent_doc_retriever(self, src_pdf_path):
+        """Create ParentDocumntRetriever from a PDF file."""
+
+        # loader = PDFPlumberLoader(src_pdf_path)
+        loader = PyPDFLoader(src_pdf_path)
+        _documents = loader.load()
+        parent_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=PARENT_CHUNK_SIZE, 
+            chunk_overlap=PARENT_CHUNK_OVERLAP
+        )
+        child_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=CHILD_CHUNK_SIZE, 
+            chunk_overlap=CHILD_CHUNK_OVERLAP
+        )
+        store = InMemoryStore()
+        embeddings = self.ollama['embedding']
+        #
+        # FIXME: Chroma처럼 바로 쉽게 만들 수는 없네. 우회적인 방법을..
+        faiss_db = FAISS.from_texts(['__a','__b'], embeddings)
+        retriever = ParentDocumentRetriever(
+            vectorstore = faiss_db,
+            docstore=store,
+            child_splitter=child_splitter,
+            parent_splitter=parent_splitter,
+            # search_type=SearchType.mmr, # default 'SearchType.similarity' FIXME: 오류발생
+            # FIXME: 아래를 하니 오히려 더 안좋아짐
+            # search_kwargs={
+            #     "k": 10,
+            #     # "lambda_mult": 0.25, # 매우 유사한 것이 많을떄, in mmr
+            #     # "fetch_k": 50, # 'mmr인 경우, 후보군 확대
+            # }
+        )
+        retriever.add_documents(_documents)
+
+        return retriever
+
+    ##
+    #
+    # CAUTION: NOTE:
+    # FIXME: 아래 load_collection은 제거 예정임. be depricated
+    # 왜냐하면, parentDocumentRetriever, ensembleRetriever등을 사용하려면
+    # on the fly로 embedding 해야 함. 물론 추후에 이런 것이 지원되면 다르겠지만.
+    #
+    def load_collections(self):
+        """Load all FAISS indexes from the vector_db_dir."""
+
+        for file in os.listdir(self.vector_db_dir):
+            if file.endswith(".faiss"):
+                index_name = file.replace(".faiss", "")
+                faiss_db = FAISS.load_local(
+                    self.vector_db_dir, 
+                    self.ollama['embedding'],
+                    index_name,
+                    allow_dangerous_deserialization=True,
+                    # kwargs={"distance_strategy":DistanceStrategy.COSINE,}
+                    distance_strategy=DistanceStrategy.COSINE
+                )
+                retriever = self.get_retriever(faiss_db, type='vectordb')
+                rag_chain = self.create_rag_chain(retriever) if retriever else None
+                template_string = (
+                    "Given the following question and context, return YES if the context is relevant to the question and NO if it isn't.\n\n"
+                    "Answer with YES or NO only. No additional text.\n\n"
+                    "> Question: {question}\n"
+                    "> Context:\n"
+                    ">>>\n"
+                    "{context}\n"
+                    ">>>\n"
+                    "> Relevant (YES / NO):"
+                )
+                prompt = PromptTemplate.from_template(template=template_string)
+                compression_retriever = ContextualCompressionRetriever(
+                    # base_compressor= LLMChainExtractor.from_llm(self.ollama["llm"]),
+                    base_compressor= LLMChainFilter.from_llm(self.ollama["llm"], prompt),
+                    base_retriever = retriever,
+                )
+                rag_chain_compression = self.create_rag_chain(compression_retriever) if compression_retriever else None
+                self.collections[index_name] = {
+                    "faiss_db": faiss_db,
+                    "retriever": retriever,
+                    "compression_retriever": compression_retriever,
+                    "rag_chain" : rag_chain,
+                    # IMPORTANT:
+                    # NOTE: MEMORY: retriever기반의 rag_chain은 거의 답변이 동일함 (local vs openai)
+                    # CAUTION: 단지 시간소요가 많이됨
+                    "rag_chain_compression":rag_chain_compression
+                }
+                #
+                logging.info(f"adding collection[{index_name}]")
+                #
--- backend/apps/rag/faiss/faiss_use.py
+++ backend/apps/rag/faiss/faiss_use.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 os_is_mac = (os.getenv("OS_SUPPORT") == "MAC") 
 
-DOC_DIR="D:\\000.Dev\\open-webui\\backend\\data"
+DOC_DIR="D:\\000.Dev\\open-webui\\backend\\apps\\rag\\faiss\\data"
 VECTOR_DIR="D:\\000.Dev\\open-webui\\backend\\data\\vector_db_faiss"
 VECTOR_DIR_OPENAI="D:\\000.Dev\\open-webui\\backend\\data\\vector_db_faiss_openai"
 VECTOR_DIR_HF="D:\\000.Dev\\open-webui\\backend\\data\\vector_db_faiss_hf"
@@ -17,7 +17,7 @@ EMBEDDING_MODEL="nomic-embed-text:latest"
 OLLAMA_URL="http://localhost:11434"
 #
 if os_is_mac:
-    DOC_DIR="/Users/bombbie/CodeLearning/open-webui/backend/apps/rag/data"
+    DOC_DIR="/Users/bombbie/CodeLearning/open-webui/backend/apps/rag/faiss/data"
     VECTOR_DIR="/Users/bombbie/CodeLearning/open-webui/backend/data/vector_db_faiss"
     VECTOR_DIR_OPENAI="/Users/bombbie/CodeLearning/open-webui/backend/data/vector_db_openai"
     VECTOR_DIR_HF="/Users/bombbie/CodeLearning/open-webui/backend/data/vector_db_hf"
@@ -29,49 +29,52 @@ logging.getLogger("faiss_client").setLevel(logging.INFO)
 
 
 faiss_client = FAISS_CLIENT(VECTOR_DIR, LLM_MODEL,EMBEDDING_MODEL,OLLAMA_URL,type="ollama")
+faiss_client.load_collection_by_ext(DOC_DIR, "pdf")
 faiss_client_openai = FAISS_CLIENT(VECTOR_DIR_OPENAI, LLM_MODEL,EMBEDDING_MODEL,OLLAMA_URL,type="openai")
+faiss_client_openai.load_collection_by_ext(DOC_DIR, "pdf")
 print(f"ollama -> {faiss_client.get_collections()}")
 print(f"openai -> {faiss_client_openai.get_collections()}")
 if not os_is_mac:
     faiss_client_hf = FAISS_CLIENT(VECTOR_DIR_HF, LLM_MODEL,EMBEDDING_MODEL,OLLAMA_URL,type="huggingface")
+    faiss_client_hf.load_collection_by_ext(DOC_DIR, "pdf")
     print(f"hf -> {faiss_client_hf.get_collections()}")
 
 
 # '9163575225d8baded4dcef8a4e71068f553f88613bd3856b8e4546128949687'
-faiss_client.create_collection("./data/회사사규(2023.11) - 복사본.pdf")
-faiss_client_openai.create_collection("./data/회사사규(2023.11) - 복사본.pdf")
-if not os_is_mac:
-    faiss_client_hf.create_collection("./data/회사사규(2023.11) - 복사본.pdf")
+# faiss_client.create_collection("./data/회사사규(2023.11) - 복사본.pdf")
+# faiss_client_openai.create_collection("./data/회사사규(2023.11) - 복사본.pdf")
+# if not os_is_mac:
+#     faiss_client_hf.create_collection("./data/회사사규(2023.11) - 복사본.pdf")
 
 #  '22069beb988b6937d65a5fcc993c0b7f4fcd3cf67a319567fc2349da408e6e2'
-faiss_client.create_collection("./data/WW챗봇 경조금 및 복리후생비 지급 기준_2024_재무관리실.pdf")
-faiss_client_openai.create_collection("./data/WW챗봇 경조금 및 복리후생비 지급 기준_2024_재무관리실.pdf")
-if not os_is_mac:
-    faiss_client_hf.create_collection("./data/WW챗봇 경조금 및 복리후생비 지급 기준_2024_재무관리실.pdf")
+# faiss_client.create_collection("./data/WW챗봇 경조금 및 복리후생비 지급 기준_2024_재무관리실.pdf")
+# faiss_client_openai.create_collection("./data/WW챗봇 경조금 및 복리후생비 지급 기준_2024_재무관리실.pdf")
+# if not os_is_mac:
+#     faiss_client_hf.create_collection("./data/WW챗봇 경조금 및 복리후생비 지급 기준_2024_재무관리실.pdf")
 
 # 'cb00c3509b954f29582a0edf61a1e70a2b139e66509622e85287bef32b1fd66'
-faiss_client.create_collection("./data/WW챗봇 출장여비규정_20240401_재무관리실.pdf")
-faiss_client_openai.create_collection("./data/WW챗봇 출장여비규정_20240401_재무관리실.pdf")
-if not os_is_mac:
-    faiss_client_hf.create_collection("./data/WW챗봇 출장여비규정_20240401_재무관리실.pdf")
+# faiss_client.create_collection("./data/WW챗봇 출장여비규정_20240401_재무관리실.pdf")
+# faiss_client_openai.create_collection("./data/WW챗봇 출장여비규정_20240401_재무관리실.pdf")
+# if not os_is_mac:
+#     faiss_client_hf.create_collection("./data/WW챗봇 출장여비규정_20240401_재무관리실.pdf")
 
 # '508efc958778204b379a3f8b2c3b7ac69bd658e67b8aa9e3c29cdaab523c39b'
-faiss_client.create_collection("./data/복귀자 관련 문의 사항.pdf")
-faiss_client_openai.create_collection("./data/복귀자 관련 문의 사항.pdf")
-if not os_is_mac:
-    faiss_client_hf.create_collection("./data/복귀자 관련 문의 사항.pdf")
+# faiss_client.create_collection("./data/복귀자 관련 문의 사항.pdf")
+# faiss_client_openai.create_collection("./data/복귀자 관련 문의 사항.pdf")
+# if not os_is_mac:
+#     faiss_client_hf.create_collection("./data/복귀자 관련 문의 사항.pdf")
 
 # '2ee2ec5a414ae38937393a281cd327345f53364b674dd338a0ebcfd2de0f632'
-faiss_client.create_collection("./data/AI시장분석_증권.pdf")
-faiss_client_openai.create_collection("./data/AI시장분석_증권.pdf")
-if not os_is_mac:
-    faiss_client_hf.create_collection("./data/AI시장분석_증권.pdf")
+# faiss_client.create_collection("./data/AI시장분석_증권.pdf")
+# faiss_client_openai.create_collection("./data/AI시장분석_증권.pdf")
+# if not os_is_mac:
+#     faiss_client_hf.create_collection("./data/AI시장분석_증권.pdf")
 
 # collection_name = 'e61cc96091a2fbdb89ffc1832e03946cc0919f0b20de97b264fddf40f70913c'
-faiss_client.create_collection("./data/생성AI 확산에 따른 디지털 인재 양성 개선 방안 연구.pdf")
-faiss_client_openai.create_collection("./data/생성AI 확산에 따른 디지털 인재 양성 개선 방안 연구.pdf")
-if not os_is_mac:
-    faiss_client_hf.create_collection("./data/생성AI 확산에 따른 디지털 인재 양성 개선 방안 연구.pdf")
+# faiss_client.create_collection("./data/생성AI 확산에 따른 디지털 인재 양성 개선 방안 연구.pdf")
+# faiss_client_openai.create_collection("./data/생성AI 확산에 따른 디지털 인재 양성 개선 방안 연구.pdf")
+# if not os_is_mac:
+#     faiss_client_hf.create_collection("./data/생성AI 확산에 따른 디지털 인재 양성 개선 방안 연구.pdf")
 
 
 # query = "휴일 및 휴가에 대한 규정은?"  
@@ -82,11 +85,11 @@ if not os_is_mac:
 # query = "프로젝트 복귀자는 어떻게 하면 되나요?" # good quality 복귀자 관련 문의 사항.pdf에서는 최고의 품질
 # query = "자녀 출생시 경조금 지급기준은? " # pdf 문서가 짧아서 조회가 잘 되는가? 하여간 good quality
 #
-query = "플리토 현황과 주식 성장 가능성 " # 언급된 부분이 일부 조회됨 not good
-collection_name = "2ee2ec5a414ae38937393a281cd327345f53364b674dd338a0ebcfd2de0f632"
+# query = "플리토 현황과 주식 성장 가능성 " # 언급된 부분이 일부 조회됨 not good
+# collection_name = "2ee2ec5a414ae38937393a281cd327345f53364b674dd338a0ebcfd2de0f632"
 #
-# query = "소프트 스킬에 대한 요구가 경력별로 어떻게 다른가?" # context compression하면 대충 비슷한 듯
-# collection_name="e61cc96091a2fbdb89ffc1832e03946cc0919f0b20de97b264fddf40f70913c"
+query = "소프트 스킬에 대한 요구가 경력별로 어떻게 다른가?" # context compression하면 대충 비슷한 듯
+collection_name="e61cc96091a2fbdb89ffc1832e03946cc0919f0b20de97b264fddf40f70913c"
 
 #
 # FAISS native
@@ -95,13 +98,18 @@ collection_name = "2ee2ec5a414ae38937393a281cd327345f53364b674dd338a0ebcfd2de0f6
 #     print("------- after faiss_client.search -----------------")
 #     print(f"<<<{index}>>>{doc}")
 
-docs = faiss_client.search_compression(query,collection_name)
+# docs = faiss_client.search_compression(query,collection_name)
+# for index, doc in enumerate(docs):
+#     print("------- after faiss_client.search_compression -----------------")
+#     print(f"<<<{index}>>>{doc}")
+# print("\n\n\n\n")
+
+
+docs = faiss_client.search_parent_doc(query,collection_name)
 for index, doc in enumerate(docs):
-    print("------- after faiss_client.search_compression -----------------")
+    print("------- after faiss_client.search_parent_doc -----------------")
     print(f"<<<{index}>>>{doc}")
 print("\n\n\n\n")
-
-
 
 
 #
@@ -112,9 +120,15 @@ print("\n\n\n\n")
 #     print("------- after faiss_openai_client.search -----------------")
 #     print(f"<<<{index}>>>{doc}")
 
-docs_openai = faiss_client_openai.search_compression(query,collection_name)
+# docs_openai = faiss_client_openai.search_compression(query,collection_name)
+# for index, doc in enumerate(docs_openai):
+#     print("------- after faiss_openai_client.search_compression -----------------")
+#     print(f"<<<{index}>>>{doc}")
+# print("\n\n\n\n")
+
+docs_openai = faiss_client_openai.search_parent_doc(query,collection_name)
 for index, doc in enumerate(docs_openai):
-    print("------- after faiss_openai_client.search_compression -----------------")
+    print("------- after faiss_openai_client.search_parent_doc -----------------")
     print(f"<<<{index}>>>{doc}")
 print("\n\n\n\n")
 
@@ -127,9 +141,13 @@ if not os_is_mac:
     # for index, doc in enumerate(docs_hf):
     #     print("------- after faiss_huggingface_client.search -----------------")
     #     print(f"<<<{index}>>>{doc}")
-    docs_hf = faiss_client_hf.search_compression(query,collection_name)
+    # docs_hf = faiss_client_hf.search_compression(query,collection_name)
+    # for index, doc in enumerate(docs_hf):
+    #     print("------- after faiss_huggingface_client.search_compression -----------------")
+    #     print(f"<<<{index}>>>{doc}")
+    docs_hf = faiss_client_hf.search_parent_doc(query,collection_name)
     for index, doc in enumerate(docs_hf):
-        print("------- after faiss_huggingface_client.search_compression -----------------")
+        print("------- after faiss_huggingface_client.search_parent_doc -----------------")
         print(f"<<<{index}>>>{doc}")
 
 #
@@ -137,33 +155,47 @@ if not os_is_mac:
 #
 #
 #
+# print("\n\n\n\n")
+# docs = faiss_client.search_llm(query,collection_name)
+# print("------- after faiss_client.search_llm -----------------")
+# print(docs['answer'])
+
+# docs_openai = faiss_client_openai.search_llm(query,collection_name)
+# print("------- after faiss_openai_client.search_llm -----------------")
+# print(docs_openai['answer'])
+
+# if not os_is_mac:
+#     docs_hf = faiss_client_hf.search_llm(query,collection_name)
+#     print("------- after faiss_huggingface_client.search_llm -----------------")
+#     print(docs['answer'])
+
+#
+#
+#
+# print("\n\n\n\n")
+# docs = faiss_client.search_llm_compression(query,collection_name)
+# print("------- after faiss_client.search_llm_compression -----------------")
+# print(docs['answer'])
+
+# docs_openai = faiss_client_openai.search_llm_compression(query,collection_name)
+# print("------- after faiss_openai_client.search_llm_compression -----------------")
+# print(docs_openai['answer'])
+
+# if not os_is_mac:
+#     docs_hf = faiss_client_hf.search_llm_compression(query,collection_name)
+#     print("------- after faiss_huggingface_client.search_llm_compression -----------------")
+#     print(docs['answer'])
+
 print("\n\n\n\n")
-docs = faiss_client.search_llm(query,collection_name)
-print("------- after faiss_client.search_llm -----------------")
+docs = faiss_client.search_llm_parent_doc(query,collection_name)
+print("------- after faiss_client.search_llm_parent_doc -----------------")
 print(docs['answer'])
 
-docs_openai = faiss_client_openai.search_llm(query,collection_name)
-print("------- after faiss_openai_client.search_llm -----------------")
+docs_openai = faiss_client_openai.search_llm_parent_doc(query,collection_name)
+print("------- after faiss_openai_client.search_llm_parent_doc -----------------")
 print(docs_openai['answer'])
 
 if not os_is_mac:
-    docs_hf = faiss_client_hf.search_llm(query,collection_name)
-    print("------- after faiss_huggingface_client.search_llm -----------------")
-    print(docs['answer'])
-
-#
-#
-#
-print("\n\n\n\n")
-docs = faiss_client.search_llm_compression(query,collection_name)
-print("------- after faiss_client.search_llm_compression -----------------")
-print(docs['answer'])
-
-docs_openai = faiss_client_openai.search_llm_compression(query,collection_name)
-print("------- after faiss_openai_client.search_llm_compression -----------------")
-print(docs_openai['answer'])
-
-if not os_is_mac:
-    docs_hf = faiss_client_hf.search_llm_compression(query,collection_name)
-    print("------- after faiss_huggingface_client.search_llm_compression -----------------")
+    docs_hf = faiss_client_hf.search_llm_parent_doc(query,collection_name)
+    print("------- after faiss_huggingface_client.search_llm_parent_doc -----------------")
     print(docs['answer'])
